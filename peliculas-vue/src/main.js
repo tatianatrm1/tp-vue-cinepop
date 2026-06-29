3bd064cbb85ad684f83a16225a72a22ffe88ee0c
Tatiana <machadotatiana19q@gmail.com>
2026-06-29 19:57:50 -0300

    .

--- peliculas-vue/src/main.js
+++ peliculas-vue/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css' // 👈 IMPORTANTE
+import '@mdi/font/css/materialdesignicons.css' //
 
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
